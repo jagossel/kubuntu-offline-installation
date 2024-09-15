381746ee218c4494a8abafe2c1ee0e92c1fe5bbf
Jonathon Gosselin <jgosselin@startmail.com>
2024-09-15 11:14:07 -0400

    Added package verification before and after copy.

--- src/offline-machine/install-packages.sh
+++ src/offline-machine/install-packages.sh
@@ -5,6 +5,18 @@ bail() {
 	exit 1
 }
 
+verifyChecksums() {
+	while read -r checksum filename; do
+		if [ $filename != "sha256sum" ]; then
+			if ! echo "$checksum  $filename" | sha256sum --check --status; then
+				bail "Chcecksum verifcation failed for $filename"
+			fi
+		fi
+	done < "sha256sum"
+
+	echo "All checksums verified successfully."
+}
+
 baseDir=$( dirname "$( readlink -f $0 )" )
 srcRootDir=$( dirname $baseDir )
 rootDir=$( dirname $srcRootDir )
@@ -16,8 +28,18 @@ packageListFilePath="$srcRootDir/packages.txt"
 localRepoPath="/usr/share/repos/local-repo"
 [ -d $localRepoPath ] || mkdir -p $localRepoPath
 
+echo "Verifying packages before copying..."
+pushd $packagesDir
+verifyChecksums
+popd
+
 echo "Copying packages to $localRepoPath..."
 cp $packagesDir/* $localRepoPath -Rf
+
+echo "Verifying packages after copying..."
+pushd $localRepoPath
+verifyChecksums
+popd
 
 keyringsPath="/etc/apt/keyrings"
 [ -d $keyringsPath ] || install -d -m 0755 /etc/apt/keyrings
