358b76e990eb9f8fedc4f0d06d61b2ff3c088e2c
Jonathon Gosselin <jgosselin@startmail.com>
2024-09-21 17:58:55 -0400

    Add in chroot to clean script

--- src/clean.sh
+++ src/clean.sh
@@ -3,4 +3,4 @@
 base_dir=$( dirname "$( readlink -f $0 )" )
 root_dir=$( dirname "$base_dir" )
 
-rm $root_dir/{keyrings,packages,preferences.d,sources.list.d} -Rfv
+rm $root_dir/{keyrings,packages,preferences.d,sources.list.d,chroot} -Rfv
