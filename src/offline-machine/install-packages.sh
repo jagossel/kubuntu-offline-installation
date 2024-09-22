778c906ae393ff74bd51aeae6bae393f3c512e71
Jonathon Gosselin <jgosselin@startmail.com>
2024-09-22 16:59:50 -0400

    Standardized offline install script

--- src/offline-machine/install-packages.sh
+++ src/offline-machine/install-packages.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-bail() {
-	echo >&2 "$@"
-	exit 1
-}
-
 if [ -z "$1" ]; then
 	echo >&2 "The profile name is required."
 	exit 1
 fi
 
-verifyChecksums() {
+verify_checksums() {
 	while read -r checksum filename; do
 		if [ $filename != "sha256sum" ]; then
 			if ! echo "$checksum  $filename" | sha256sum --check --status; then
-				bail "Chcecksum verifcation failed for $filename"
+				echo >&2 "Checksum verification failed for $filename"
+				exit 1
 			fi
 		fi
 	done < "sha256sum"
@@ -22,68 +18,70 @@ verifyChecksums() {
 	echo "All checksums verified successfully."
 }
 
-baseDir=$( dirname "$( readlink -f $0 )" )
-srcRootDir=$( dirname $baseDir )
-rootDir=$( dirname $srcRootDir )
-packagesDir="$rootDir/packages"
+base_dir=$( dirname "$( readlink -f $0 )" )
+src_root_dir=$( dirname $base_dir )
+root_dir=$( dirname $src_root_dir )
+packages_dir="$root_dir/packages"
 
-packageListFilePath="$srcRootDir/packages.csv"
-[ -f $packageListFilePath ] || bail "Cannot find the package list file, $packageListFilePath."
+package_list_file_path="$src_root_dir/packages.csv"
+if [ ! -f $package_list_file_path ]; then
+	echo >&2 "Cannot find the package list file, $package_list_file_path."
+	exit 1
+fi
 
-localRepoPath="/usr/share/repos/local-repo"
-[ -d $localRepoPath ] || mkdir -p $localRepoPath
+local_repo_path="/usr/share/repos/local-repo"
+[ -d $local_repo_path ] || mkdir -p $local_repo_path
 
 echo "Verifying packages before copying..."
-pushd $packagesDir
-verifyChecksums
+pushd $packages_dir
+verify_checksums
 popd
 
-echo "Copying packages to $localRepoPath..."
-cp $packagesDir/* $localRepoPath -Rf
+echo "Copying packages to $local_repo_path..."
+cp $packages_dir/* $local_repo_path -Rf
 
 echo "Verifying packages after copying..."
-pushd $localRepoPath
-verifyChecksums
+pushd $local_repo_path
+verify_checksums
 popd
 
-keyringsPath="/etc/apt/keyrings"
-[ -d $keyringsPath ] || install -d -m 0755 /etc/apt/keyrings
+keyrings_path="/etc/apt/keyrings"
+[ -d $keyrings_path ] || install -d -m 0755 /etc/apt/keyrings
 
-sourceKeyringsPath="$rootDir/keyrings"
-if [ -d $sourceKeyringsPath ]; then
+source_keyrings_path="$root_dir/keyrings"
+if [ -d $source_keyrings_path ]; then
 	echo "Copying keyrings..."
-	cp $sourceKeyringsPath/* $keyringsPath -Rf
+	cp $source_keyrings_path/* $keyrings_path -Rf
 fi
 
-sourceListPath="/etc/apt/sources.list.d"
-[ -d $sourceListPath ] || mkdir -p $sourceListPath
+source_list_path="/etc/apt/sources.list.d"
+[ -d $source_list_path ] || mkdir -p $source_list_path
 
-additionalSourcesPath="$rootDir/sources.list.d"
-if [ -d $additionalSourcesPath ]; then
+additional_sources_path="$root_dir/sources.list.d"
+if [ -d $additional_sources_path ]; then
 	echo "Copying additional repo sources..."
-	cp $additionalSourcesPath/* $sourceListPath -Rf
+	cp $additional_sources_path/* $source_list_path -Rf
 fi
 
-preferencesPath="/etc/apt/preferences.d"
+preferences_path="/etc/apt/preferences.d"
 
-additionalPreferencesPath="$rootDir/preferences.d"
-if [ -d $additionalPreferencesPath ]; then
+additional_preferences_path="$rootDir/preferences.d"
+if [ -d $additional_preferences_path ]; then
 	echo "Copying preferences..."
-	cp $additionalPreferencesPath $preferencesPath -Rf
+	cp $additional_preferences_path $preferences_path -Rf
 fi
 
 echo "Generating repo meta data..."
-echo "deb [trusted=yes] file:$localRepoPath ./" > /etc/apt/sources.list.d/offline.list
+echo "deb [trusted=yes] file:$local_repo_path ./" > /etc/apt/sources.list.d/offline.list
 
 apt-get update
 apt-get -y upgrade
 
-# packageList="$( grep ".*" $packageListFilePath|tr '\n' ' ' )"
-packageList=$( grep -E ".*,($1|\*)" $packageListFilePath | awk -F, '{ print $1 }' | tr '\n' ' ' )
-apt-get -y install $packageList
+package_list=$( grep -E ".*,($1|\*)" $package_list_file_path | awk -F, '{ print $1 }' | tr '\n' ' ' )
+apt-get -y install $package_list
 
 # Install SDR++
-dpkg --install $localRepoPath/sdrpp_ubuntu_noble_amd64.deb
+dpkg --install $local_repo_path/sdrpp_ubuntu_noble_amd64.deb
 apt-get --fix-broken -y install
 
 qdbus org.kde.Shutdown /Shutdown logoutAndReboot
