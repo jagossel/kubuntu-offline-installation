336005d94dad9a66cbad90935a5b8545a8ee232f
Jonathon Gosselin <jgosselin@startmail.com>
2024-10-05 17:59:51 -0400

    Standardized Verify Packages Script

--- src/online-machine/verify-packages.sh
+++ src/online-machine/verify-packages.sh
@@ -3,24 +3,24 @@ bail() {
 	exit 1
 }
 
-baseDir=$( dirname "$( readlink -f $0 )" )
-srcRootDir=$( dirname $baseDir )
-rootDir=$( dirname $srcRootDir )
-packagesDir="$rootDir/packages"
-checksumPath="$packagesDir/sha256sum"
+base_dir=$( dirname "$( readlink -f $0 )" )
+src_root_dir=$( dirname $base_dir )
+root_dir=$( dirname $src_root_dir )
+packages_dir="$root_dir/packages"
+checksum_path="$packages_dir/sha256sum"
 
-[ -d $packagesDir ] || bail "The packages directory does not exist.  Run the prepare-packages.sh script first."
-[ -f $checksumPath ] || bail "The checksum file does not exist.  Run the prepare-packages.sh script first."
+[ -d $packages_dir ] || bail "The packages directory does not exist.  Run the prepare-packages.sh script first."
+[ -f $checksum_path ] || bail "The checksum file does not exist.  Run the prepare-packages.sh script first."
 
 echo "Verifying packages..."
-pushd $packagesDir
+pushd $packages_dir
 while read -r checksum filename; do
 	if [ $filename != "sha256sum" ]; then
 		if ! echo "$checksum  $filename" | sha256sum --check --status; then
 			bail "Chcecksum verifcation failed for $filename"
 		fi
 	fi
-done < "$checksumPath"
+done < "$checksum_path"
 popd
 
 echo "All checksums verified successfully."
